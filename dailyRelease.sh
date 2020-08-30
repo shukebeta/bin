8de3791cba26ccf683f27e1085070d66c03b13cc
David Wei <david.wei@koalatech.co.nz>
2020-08-31 09:42:40 +1200

    daily improve

--- dailyRelease.sh
+++ dailyRelease.sh
@@ -7,15 +7,21 @@ git checkout master
 git pull --ff
 git fetch
 git merge origin/develop
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
 	exportAllDbStructure
 	exportalldbdict.sh
+    git add .
 	git commit -am"automatically save db structure @ `date '+%Y-%m-%d %H:%M:%S'`"
 	git push
+else
+    echo '!!!!!!!!!!!!!!!!Something wrong!'
 fi
 cd ~/bin/git/front_app
 git checkout master
 git pull --ff
 git fetch
 git merge origin/develop
+if [ $? -ne 0 ]; then
+    echo '!!!!!!!!!!!!!!!!Something wrong @ front end!'
+fi
 git push
