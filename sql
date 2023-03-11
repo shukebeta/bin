d0980b464e32f5bc04ce3d549c0887da071f51c6
DW at xps <zhw7@protonmail.ch>
2023-03-11 14:07:40 +1300

    2023 update

---
+++ sql
@@ -0,0 +1,1 @@
+~/3rd/sqlcl/bin/sql -L system/oracle@//localhost:1521/xe
---
+++ sync-prod-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+# source ~/bin/manual_backup.sh
+#lastFile=$(echo $(ssh centos 'echo $(find ~/backups/mysql -ctime -1|tail -1)'))
+#scp centos:"${lastFile}" /tmp/tmp.sql.gz
+#rm /tmp/tmp.sql || true
+cd ~ && gzip -d ~/tmp.sql.gz && mysql < ./tmp.sql && echo 'Well done' ||(echo 'Something wrong!' && exit 1)
+
+
